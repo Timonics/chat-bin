e704d5a58f4757cdf2779d89c71474b4ffb22610
Oderinde Michael <Olubiyioderinde7@gmail.com>
2025-10-13 22:03:57 +0100

    Refactor code structure for improved readability and maintainability

--- dist/queue/workers/secret-cleanup.module.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { ConnectionOptions } from 'bullmq';
-import { PrismaService } from 'src/prisma/prisma.service';
-export declare class SecretCleanupModule implements OnModuleInit {
-    private readonly prisma;
-    private readonly connection;
-    constructor(prisma: PrismaService, connection: ConnectionOptions);
-    onModuleInit(): Promise<void>;
-}
--- dist/queue/workers/secret-cleanup.worker.d.ts
+++
@@ -1,3 +0,0 @@
-import { ConnectionOptions, Worker } from 'bullmq';
-import { PrismaService } from 'src/prisma/prisma.service';
-export declare function startSecretCleanupWorker(prismaService: PrismaService, connection: ConnectionOptions): Worker<any, any, string>;
